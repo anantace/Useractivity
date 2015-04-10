9a56599695d83a822212df3a571d65a320f09fc1
asudau <sudau@elan-ev.de>
2015-04-10 12:11:24 +0200

    LastOnline
    -Usernamen und letzter Onlinezeitpunkt werden angezeigt

--- controllers/show.php
+++ controllers/show.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once 'app/controllers/news.php';
-
+require_once 'lib/classes/DBManager.class.php';
 
 class ShowController extends StudipController {
 
@@ -35,12 +34,53 @@ class ShowController extends StudipController {
     }
 
     public function index_action() {
-	
+	$stmt = DBManager::get()->prepare("SELECT su.user_id,  uo.last_lifesign FROM seminar_user su LEFT JOIN user_online uo ON (su.user_id = uo.user_id)
+					WHERE su.status = 'autor' AND su.Seminar_id = ?");
+	 $stmt->execute(array($this->getSeminarID()));
+	 //$count = $stmt->rowCount();
+	 $result = $stmt->fetch();
+	$this->user = $this->getUser();
+
+
 	
 
     }
+	
+    private function getUser() {
+	$user;
+	$stmt = DBManager::get()->prepare("SELECT su.user_id, FROM_UNIXTIME(uo.last_lifesign) as lifesign, au.Vorname, au.Nachname FROM seminar_user su 
+					       LEFT JOIN user_online uo ON (su.user_id = uo.user_id)
+						LEFT JOIN auth_user_md5 au ON (au.user_id = su.user_id)
+					WHERE su.status IN ('autor', 'dozent') 
+       			       AND su.Seminar_id = ? 
+					ORDER BY uo.last_lifesign DESC");
+	 $stmt->execute(array($this->getSeminarID()));
+	 $result = $stmt->fetchAll();
+	 foreach($result as $rs){
+	 	$user[] = array('id' => $rs['user_id'],
+				  'lifesign' => $rs['lifesign'],
+				  'vorname' => $rs['Vorname'],
+				  'nachname' => $rs['Nachname']);
+	}
+	//$count = $stmt->rowCount();
+	 return $user;
+	 //$result['seminar_id']
 
+    }
 
-    
+    static function getSeminarId()
+    {
+        if (!Request::option('cid')) {
+            if ($GLOBALS['SessionSeminar']) {
+                URLHelper::bindLinkParam('cid', $GLOBALS['SessionSeminar']);
+                return $GLOBALS['SessionSeminar'];
+            }
+
+            return false;
+        }
+
+        return Request::option('cid');
+    }
+
 
 }
