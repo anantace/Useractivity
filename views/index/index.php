534ac4078a1c35ba46bd56c566ce61a01152d9ed
asudau <sudau@elan-ev.de>
2017-09-29 12:37:09 +0200

    Show Badges with Useractivity

--- views/index/index.php
+++ views/index/index.php
@@ -8,10 +8,11 @@
 <table id="keywords" class="tablesorter">
     <thead>
 		<tr>
-        <th><span>Name</span></th>
+        <th style='width:25%'><span>Name</span></th>
         <th><span>Zuletzt online vor</span></th>
-        <th style='display:none'><span>Zuletzt online vor</span></th>
-        <th><span>Anzahl Forenbeiträge</span></th>
+        <th style='display:none; width:25%'><span>Zuletzt online vor</span></th>
+        <th style='width:5%'><span>Anzahl Forenbeiträge</span></th>
+        <th style='width:45%'><span>Badges</span></th>
         <!--<th>Courseware besucht?</th>-->
     </tr>
     </thead>
@@ -20,6 +21,7 @@
     <?
     
     $currentTime = time();
+    $badge_content;
     
     foreach ($tn_data as $tn){ 
     
@@ -28,12 +30,25 @@
         if (round($difference/(60*60*24),0) > 1000){
             $last_online = 'noch nie';
         }
+
+        if($badges[$tn['user_id']]){
+            foreach ($badges[$tn['user_id']] as $badge){
+                $block = new \Mooc\DB\Block($badge['badge_block_id']);
+                $field = current(Mooc\DB\Field::findBySQL('block_id = ? AND name = ?', array($block->id, 'file_id')));
+                $file_id= $field->content;
+                $field = current(\Mooc\DB\Field::findBySQL('block_id = ? AND name = ?', array($block->id, 'file_name')));
+                $file_name = $field->content;
+                $badge_content[$tn['user_id']] .= 
+                '<img title=\'' . date('d.m.Y', $badge['mkdate']) . '\' style=\'max-width:10%\' src=\'../../sendfile.php?type=0&file_id=' . $file_id . '&file_name=' . $file_name . '\'/>';
+            }
+        }
         ?>
         <tr>
             <td><?= $tn['Vorname'] . ' ' . $tn['Nachname']?></td>
             <td style='display:none'><?= $tn['last_lifesign']?></td>
             <td><?= $last_online ?></td>
             <td><?= $tn['Forenbeitraege']?></td>
+            <td><?= $badge_content[$tn['user_id']]?></td>
             <!--<td>Courseware besucht?</td>-->
         </tr>
         <?
@@ -48,6 +63,7 @@
 		<tr>
                     <th><span>Name</span></th>
         <th><span>Zuletzt online vor</span></th>
+        <th style='display:none'><span>Zuletzt online vor</span></th>
         <th><span>Anzahl Forenbeiträge</span></th>
         <!--<th>Courseware besucht?</th>-->
     </tr>
@@ -68,7 +84,7 @@
         ?>
         <tr>
             <td><?= $dz['Vorname'] . ' ' . $dz['Nachname']?></td>
-            <!--<td><?= date("d.m.Y - G:i",$dz['last_lifesign'])?></td>-->
+            <td style='display:none'><?= $dz['last_lifesign']?></td>
             <td><?= $last_online ?></td>
             <td><?= $dz['Forenbeitraege']?></td>
             <!--<td>Courseware besucht?</td>-->
