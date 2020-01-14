ac4d15e6e195c5c2d3bcd0bdf4599121e8e55ea0
asudau <sudau@elan-ev.de>
2020-01-14 11:17:25 +0100

    changes for SimpleOrMap configure/construct

--- models/ActivitySettings.class.php
+++ models/ActivitySettings.class.php
@@ -11,18 +11,12 @@ class ActivitySettings extends SimpleORMap
 
     public $errors = array();
 
-    /**
-     * Give primary key of record as param to fetch
-     * corresponding record from db if available, if not preset primary key
-     * with given value. Give null to create new record
-     *
-     * @param mixed $id primary key of table
-     */
-    public function __construct($id = null) {
-
-        $this->db_table = 'activity_settings';
-
-        parent::__construct($id);
+    
+    protected static function configure($config = array())
+    {
+        $config['db_table'] = 'activity_settings';
+        parent::configure($config);
     }
+
 }
     
